9af54490047ca38d8a2e087a1420a9eb3c7b8ed9
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2021-09-12 17:05:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "敬爱的老师": "./img/xiaokeai.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -38,6 +38,6 @@ var config = {
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "给老师的一封信",
     }
 };
